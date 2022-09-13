2a6d7e8962bdecca45e712581b82311f62ad47bd
Paulos Tadesse <paulosta952@gmail.com>
2022-09-13 15:04:24 +0300

    :zap: initial commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,13 @@
+<template>
+ <div class="text-4xl text-red-400 font-bold text-center"> Arki Home page Welcome !</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
